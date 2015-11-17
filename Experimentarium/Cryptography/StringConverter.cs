2bf837d7eb513ef4caceae7411f1b349afc95863
Andrey Andreev <AndreevAndrey@ya.ru>
2015-11-17 17:53:45 +0300

    Some functionality with xor and strings

---
+++ Experimentarium/Cryptography/StringConverter.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+
+namespace Experimentarium.Cryptography
+{
+    public class StringConverter
+    {
+        public static byte[] ConvertHexStringToBytes(string hexString)
+        {
+            if (hexString.Length % 2 != 0)
+            {
+                throw new ArgumentException("Number of chars in hex string must be even");
+            }
+
+            byte[] bytes = new byte[hexString.Length / 2];
+
+            for (int i = 0, j = 0; i < hexString.Length; i += 2, j++)
+            {
+                string hexSubstring = hexString.Substring(i, 2);
+                byte b;
+                if (Byte.TryParse(hexSubstring, NumberStyles.HexNumber, null, out b))
+                {
+                    bytes[j] = b;
+                }
+                else
+                {
+                    throw new ArgumentException($"HexString contains non-hex value: {hexSubstring}");
+                }
+            }
+
+            return bytes;
+        }
+
+        public static string ConvertBytesToHexString(byte[] bytes)
+        {
+            var result = new StringBuilder(bytes.Length);
+
+            foreach (byte b in bytes)
+            {
+                result.Append(b.ToString("x2"));
+            }
+
+            return result.ToString();
+        }
+
+        public static string ConvertHexStringToAsciiString(string hexString)
+        {
+            return Encoding.ASCII.GetString(ConvertHexStringToBytes(hexString));
+        }
+
+        public static string ConvertBytesToAsciiString(byte[] bytes)
+        {
+            StringBuilder result = new StringBuilder(bytes.Length);
+
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                string s = Encoding.ASCII.GetString(bytes, i, 1);
+                if ((String.CompareOrdinal(s, "A") >= 0 && String.CompareOrdinal(s, "Z") <= 0) || 
+                    (String.CompareOrdinal(s, "a") >= 0 && String.CompareOrdinal(s, "z") <= 0))
+                {
+                    result.Append(s);
+                }
+                else
+                {
+                    result.Append("_");
+                }
+            }
+
+            return result.ToString();
+        }
+    }
+}
---
+++ Experimentarium/Cryptography/XorHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Experimentarium.Cryptography
+{
+    public class XorHelper
+    {
+        public static string XorHexStrings(string first, string second)
+        {
+            byte[] xoredBytes = XorHexStringsToBytes(first, second);
+
+            string xoredString = StringConverter.ConvertBytesToHexString(xoredBytes);
+
+            return xoredString;
+        }
+
+        public static byte[] XorHexStringsToBytes(string first, string second)
+        {
+            if (first.Length != second.Length)
+            {
+                throw new ArgumentException("Both strings must have same size");
+            }
+
+            byte[] firstBytes = StringConverter.ConvertHexStringToBytes(first);
+            byte[] secondBytes = StringConverter.ConvertHexStringToBytes(second);
+
+            byte[] xoredBytes = new byte[firstBytes.Length];
+
+            for (int i = 0; i < firstBytes.Length; i++)
+            {
+                byte xoredByte = (byte) (firstBytes[i] ^ secondBytes[i]);
+                xoredBytes[i] = xoredByte;
+            }
+
+            return xoredBytes;
+        }
+    }
+}
